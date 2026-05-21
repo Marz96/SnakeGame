b32667b33e9a7fc1d3d06a17407f8e95fa463b35
Marz96 <101660343+Marz96@users.noreply.github.com>
2026-05-21 12:27:26 +0500

    Update Food.cs

--- Snake/Food.cs
+++ Snake/Food.cs
@@ -11,15 +11,14 @@ namespace SnakeGame
         public Point Position { get; private set; }
         private Random _random = new Random();
 
-        public void Spawn(List<Point> snakeBody)
-        {
-            while (true)
-            {
-                Position = new Point(_random.Next(GlobalSettings.GridWidth), _random.Next(GlobalSettings.GridHeight));
-                if (!snakeBody.Contains(Position)) break;
-            }
-        }
-
+        public void Spawn(List<Point> snakeBody, List<Point> obstacles)
+{
+    while (true)
+    {
+        Position = new Point(_random.Next(GlobalSettings.GridWidth), _random.Next(GlobalSettings.GridHeight));
+        if (!snakeBody.Contains(Position) && !obstacles.Contains(Position)) break;
+    }
+}
         public void Draw(SpriteBatch spriteBatch, Texture2D texture)
         {
             spriteBatch.Draw(texture, new Rectangle(
